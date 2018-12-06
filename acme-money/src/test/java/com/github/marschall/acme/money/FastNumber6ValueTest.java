de8313f1ee9fbf74cad8112ddfb146c48965c2ee
Philippe Marschall <philippe.marschall@netcetera.com>
2018-12-06 14:14:17 +0100

    organize imports

--- acme-money/src/test/java/com/github/marschall/acme/money/FastNumber6ValueTest.java
+++ acme-money/src/test/java/com/github/marschall/acme/money/FastNumber6ValueTest.java
@@ -5,7 +5,6 @@ import static org.hamcrest.Matchers.comparesEqualTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.hamcrest.Matchers.comparesEqualTo;
 
 import java.io.IOException;
 import java.math.BigDecimal;
